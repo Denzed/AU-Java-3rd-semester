9b232c1cdf1f2cd75478eeb63ab19df76884b64b
Daniil Smirnov <dansmi97@gmail.com>
2016-11-13 05:47:25 +0300

    Added hw04

---
+++ src/main/java/sp/FirstPartTasks.java
@@ -0,0 +1,121 @@
+package sp;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.function.ToDoubleFunction;
+
+public final class FirstPartTasks {
+
+    private FirstPartTasks() {}
+
+    // Список названий альбомов
+    public static List<String> allNames(Stream<Album> albums) {
+        return albums.map(Album::getName)
+        			 .collect(Collectors.toList());
+    }
+
+    // Список названий альбомов, отсортированный лексикографически по названию
+    public static List<String> allNamesSorted(Stream<Album> albums) {
+        return allNames(albums).stream()
+        					   .sorted()
+        					   .collect(Collectors.toList());
+    }
+
+    // Список треков, отсортированный лексикографически по названию, включающий все треки альбомов из 'albums'
+    public static List<String> allTracksSorted(Stream<Album> albums) {
+        return albums.map(Album::getTracks)
+        			 .flatMap(List::stream)
+        			 .map(Track::getName)
+        			 .sorted()
+        			 .collect(Collectors.toList());
+    }
+
+    // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
+    public static List<Album> sortedFavorites(Stream<Album> albums) {
+        return albums.filter((album) -> album.getTracks()
+        									 .stream()
+        									 .map(Track::getRating)
+        									 .anyMatch((rating) -> (rating > 95)))
+        			 .sorted((a1, a2) -> a1.getName().compareTo(a2.getName()))
+        			 .collect(Collectors.toList());
+    }
+
+    // Сгруппировать альбомы по артистам
+    public static Map<Artist, List<Album>> groupByArtist(Stream<Album> albums) {
+        return albums.collect(Collectors.groupingBy(Album::getArtist));
+    }
+
+    // Сгруппировать альбомы по артистам (в качестве значения вместо объекта 'Artist' использовать его имя)
+    public static Map<Artist, List<String>> groupByArtistMapName(Stream<Album> albums) {
+    	return albums.collect(Collectors.groupingBy(Album::getArtist))
+    				 .entrySet()
+    				 .stream()
+    				 .collect(Collectors.toMap(Map.Entry::getKey,
+    						 				   (entry) -> entry.getValue()
+    						 					   			   .stream()
+    						 					   			   .map(Album::getName)
+    						 					   			   .collect(Collectors.toList())));
+    }
+
+    // Число повторяющихся альбомов в потоке
+    public static long countAlbumDuplicates(Stream<Album> albums) {
+    	return albums.collect(Collectors.groupingBy((x) -> x, 
+    												Collectors.counting()))
+    			     .entrySet()
+    				 .stream()
+    				 .collect(Collectors.summingLong((entry) -> entry.getValue() - 1));
+    }
+
+    // Альбом, в котором максимум рейтинга минимален
+    // (если в альбоме нет ни одного трека, считать, что максимум рейтинга в нем --- 0)
+    public static Optional<Album> minMaxRating(Stream<Album> albums) {
+        return albums.min(Comparator
+        		.comparingInt((album) -> album.getTracks()
+        									  .stream()
+        									  .map(Track::getRating)
+        									  .max(Comparator.naturalOrder())
+        									  .orElse(0)));
+    }
+
+    // Список альбомов, отсортированный по убыванию среднего рейтинга его треков (0, если треков нет)
+    public static List<Album> sortByAverageRating(Stream<Album> albums) {
+    	ToDoubleFunction<Album> avgRating = (album) -> album.getTracks()
+    														.stream()
+    														.collect(Collectors.averagingInt(
+    																Track::getRating));
+        return albums.sorted(Comparator.comparingDouble(avgRating)
+        							   .reversed())
+        			 .collect(Collectors.toList());
+    }
+
+    // Произведение всех чисел потока по модулю 'modulo'
+    // (все числа от 0 до 10000)
+    public static int moduloProduction(IntStream stream, int modulo) {
+        return stream.reduce(1, (pref,cur) -> pref * cur % modulo);
+    }
+
+    // Вернуть строку, состояющую из конкатенаций переданного массива, и окруженную строками "<", ">"
+    // см. тесты
+    public static String joinTo(String... strings) {
+        return Arrays.asList(strings)
+        			 .stream()
+        			 .collect(Collectors.joining(", ", "<", ">"));
+    }
+
+    // Вернуть поток из объектов класса 'clazz'
+    public static <R> Stream<R> filterIsInstance(Stream<?> s, Class<R> clazz) {
+    	return s.flatMap((elem) -> {
+    		Stream.Builder<R> res = Stream.builder();
+			if (clazz.isInstance(elem)) {
+				res.accept(clazz.cast(elem));
+			}
+			return res.build();
+    	});
+    }
+}
---
+++ src/test/java/sp/SecondPartTasksTest.java
@@ -0,0 +1,61 @@
+package sp;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static sp.SecondPartTasks.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SecondPartTasksTest {
+
+    @Test
+    public void testFindQuotes() {
+    	String file = Paths.get("src", "main", "java", "sp", "SecondPartTasks.java")
+    			  		   .toString();
+        assertEquals(Arrays.asList("package sp;"),
+   			 		 findQuotes(Arrays.asList(file), "pack"));
+        assertEquals(Arrays.asList(),
+			 		 findQuotes(Arrays.asList(), "pack"));
+        assertEquals(Arrays.asList(),
+        			 findQuotes(Arrays.asList(file), "peck"));
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+        assertEquals(Math.PI / 4, piDividedBy4(), 1e-3);
+    }
+
+    @Test
+    public void testFindPrinter() {
+        assertEquals("lol", findPrinter(ImmutableMap.of("lol", Arrays.asList())));
+        assertEquals("kek", findPrinter(ImmutableMap.of("lol", Arrays.asList(), 
+														"kek", Arrays.asList("kek"))));
+        assertEquals("keks", findPrinter(ImmutableMap.of("lol", Arrays.asList(), 
+														 "kek", Arrays.asList("kek"),
+        												 "keks", Arrays.asList("k", 
+        														 			   "e", 
+        														 			   "k", 
+        														 			   "s"))));
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+        assertEquals(Collections.emptyMap(), 
+        			 calculateGlobalOrder(Collections.emptyList()));
+        assertEquals(ImmutableMap.of("a", 3), 
+   			 		 calculateGlobalOrder(Arrays.asList(ImmutableMap.of("a", 1),
+   					 								    ImmutableMap.of("a", 2))));
+        assertEquals(ImmutableMap.of("a", 1, "b", 2), 
+        			 calculateGlobalOrder(Arrays.asList(ImmutableMap.of("a", 1),
+        					 							ImmutableMap.of("b", 2))));
+    }
+}
